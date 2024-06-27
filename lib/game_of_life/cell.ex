566235447ac21e21c0c99ff2f623fd3ad473ba1a
Sarah Ella Stoltze <sstoltze@gmail.com>
2024-06-27 21:47:55 +0200

    Abstract away some cell interface

--- lib/game_of_life/cell.ex
+++ lib/game_of_life/cell.ex
@@ -1,6 +1,10 @@
 defmodule GameOfLife.Cell do
   defstruct [:contents]
 
+  def new(_x, _y, opts) do
+    %__MODULE__{contents: Keyword.get(opts, :contents)}
+  end
+
   def get(%__MODULE__{contents: contents}) do
     contents
   end
--- test/game_of_life/cell_test.exs
+++ test/game_of_life/cell_test.exs
@@ -3,7 +3,7 @@ defmodule GameOfLife.CellTest do
   alias GameOfLife.Cell
 
   test "cells let you get their contents" do
-    c = %Cell{contents: nil}
-    assert Cell.get(c) == nil
+    c = Cell.new(0, 0, contents: :a_thing)
+    assert Cell.get(c) == :a_thing
   end
 end
